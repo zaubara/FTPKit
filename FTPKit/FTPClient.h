9225a42a7113d705677c36fed57e172251ecfc81
ReDetection <redetection@gmail.com>
2016-01-19 16:22:06 +0600

    rename -connect to -newConnection, reused existing connection, quit on dealloc (untested)

--- FTPKit/FTPClient.h
+++ FTPKit/FTPClient.h
@@ -4,10 +4,6 @@
  Consider implementing more of the commands specified at:
  http://en.wikipedia.org/wiki/List_of_FTP_commands
  
- Currently this creates a new connection to the FTP server for every command
- issued. This means the state of the current working directory is NOT kept and.
- therefore, some commands are not of use.
- 
  */
 
 #import "FTPHandle.h"
