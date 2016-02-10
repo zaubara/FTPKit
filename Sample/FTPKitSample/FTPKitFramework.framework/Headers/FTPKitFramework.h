4fac9edd1b3c54f42bb710c22d2b137b28b75824
Martin Puza <m.puza@zetes.at>
2016-02-10 12:59:21 +0100

    change target to 8.0 added built framework

---
+++ Sample/FTPKitSample/FTPKitFramework.framework/Headers/FTPKitFramework.h
@@ -0,0 +1,20 @@
+//
+//  FTPKitFramework.h
+//  FTPKitFramework
+//
+//  Created by Martin Puza on 10.02.16.
+//  Copyright © 2016 Upstart Illustration LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTPKitFramework.
+FOUNDATION_EXPORT double FTPKitFrameworkVersionNumber;
+
+//! Project version string for FTPKitFramework.
+FOUNDATION_EXPORT const unsigned char FTPKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTPKitFramework/PublicHeader.h>
+
+#import "FTPKit.h"
+
